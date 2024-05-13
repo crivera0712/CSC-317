5a1dd3a606260317b39f0ab6bfff5de4eeabc134
Danny <dduenas@sfsu.edu>
2024-05-12 23:20:51 -0700

    added a signup and login, added js features, made responisve, and edited css

--- Ecommerce/wwJS/index.js
+++ Ecommerce/wwJS/index.js
@@ -12,3 +12,12 @@ if (close) {
     nav.classList.remove('active');
   })
 }
+/*signup*/
+const loginSignupLink = document.querySelectorAll('.form-box .bottom-link a');
+const formPopup = document.querySelector(".form-popup");
+loginSignupLink.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+  })
+})
