7e599806d990907cf7f3b6d02a3fe663334fc3ec
Danny <dduenas@sfsu.edu>
2024-05-13 20:10:30 -0700

    fixed some bugs

--- Ecommerce/wwJS/index.js
+++ Ecommerce/wwJS/index.js
@@ -1,5 +1,5 @@
 const hamburger = document.getElementById('hamburger');
-const close = document.getElementById('close')
+const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if (hamburger) {
@@ -12,12 +12,4 @@ if (close) {
     nav.classList.remove('active');
   })
 }
-/*signup*/
-const loginSignupLink = document.querySelectorAll('.form-box .bottom-link a');
-const formPopup = document.querySelector(".form-popup");
-loginSignupLink.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
-  })
-})
+
