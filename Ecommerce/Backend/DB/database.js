56f3ade4d6b44e4f963edbaf504b6a340f22ef96
crivera0712 <125428748+crivera0712@users.noreply.github.com>
2024-05-20 08:58:28 -0700

    Update database.js

--- Ecommerce/Backend/DB/database.js
+++ Ecommerce/Backend/DB/database.js
@@ -5,10 +5,12 @@ dotenv.config()
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB
-}).promise()
+}).promise();
+export default pool;
 
 //returns array of users
 export async function getUsers() {
